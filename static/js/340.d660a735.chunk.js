496e9e5ca04a01ab50a9b653c89ecd4d643143b5
Максим <fava7286@gmail.com>
2025-02-23 16:13:51 +0300

    Updates

---
+++ static/js/340.d660a735.chunk.js
@@ -0,0 +1,30 @@
+"use strict";(self.webpackChunkweather_app=self.webpackChunkweather_app||[]).push([[340],{340:(e,r,t)=>{t.r(r),t.d(r,{default:()=>o});t(43);var a=t(464),n=t(870),c=t(712);const s=t.p+"static/media/error404.f24ec84fbdc151be41da03c68e2a6f3d.svg";var i=t(579);const o=()=>{const e=(0,n.G)((e=>e.weather.error));return(0,i.jsxs)(d,{$error:e,$fadeIn:c.q,children:[(0,i.jsx)(l,{src:s,alt:"\u041f\u0440\u043e\u0438\u0437\u043e\u0448\u043b\u0430 \u043e\u0448\u0438\u0431\u043a\u0430 404"}),(0,i.jsx)(p,{children:"\u0423\u043f\u0441, \u043d\u0435\u043f\u0440\u0430\u0432\u0438\u043b\u044c\u043d\u043e \u0432\u0432\u0435\u0434\u0435\u043d\u0430 \u0441\u0442\u0440\u0430\u043d\u0430 \u0438\u043b\u0438 \u0433\u043e\u0440\u043e\u0434"})]})},d=a.Ay.section`
+    display: ${e=>{let{$error:r}=e;return r?"flex":"none"}};
+    opacity: 0;
+    scale: 0;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    transition: 0.5s linear;
+    padding: 0.5rem;
+
+   ${e=>{let{$error:r,$fadeIn:t}=e;return r&&a.AH` // для интерполяции
+            animation: 0.5s ${t} forwards;
+    `}}
+`,l=a.Ay.img`
+    width: 20rem;
+`,p=a.Ay.p`
+    text-align: center;
+    width: 17.5rem;
+`},712:(e,r,t)=>{t.d(r,{q:()=>a});const a=t(464).i7`
+	from {
+		opacity: 0;
+		scale: 0;
+	}
+
+	to {
+		opacity: 1;
+		scale: 1;
+	}
+`}}]);
+//# sourceMappingURL=340.d660a735.chunk.js.map
